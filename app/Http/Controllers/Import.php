37464b8a221d712c6baf3488981fae6944634ba2
inyaJohnson <intoajohnson@gmail.com>
2020-04-22 11:52:52 +0100

    Validateion update for uploads

--- app/Http/Controllers/Import.php
+++ app/Http/Controllers/Import.php
@@ -11,15 +11,15 @@ class Import extends Controller
 {
     public function import(Request $request){
         //dd($request->file);
-        
+
         $this->validate($request, [
             'file' => 'required|mimes:xls,xlsx'
-    
+
          ]);
-      
+
     // try{
      Excel::import(new LeadImport, request()->file('file'));
-    
+
         return back()->with('success', 'file uploaded successfully');
     // }catch(Exception $e){
         return back()->with('error', 'something went wrong');
@@ -34,21 +34,21 @@ class Import extends Controller
 
     public function unsub($id){
         $lead = Lead::findorfail($id);
-      
+
            $lead->stop = 'y';
            $lead->update();
            return $lead->full_name . ' you have successfully unsubscribed from receiving mails from us';
-      
+
     }
 
     public function sub($id){
         $lead = Lead::findorfail($id);
-      
-        
+
+
         $lead->stop = 'n';
         $lead->update();
-        return $lead->full_name . ' Thank you for subscribing to receiving mails from us';  
-       
+        return $lead->full_name . ' Thank you for subscribing to receiving mails from us';
+
     }
 
     public function check($id){
--- app/Imports/LeadImport.php
+++ app/Imports/LeadImport.php
@@ -11,51 +11,40 @@ use Maatwebsite\Excel\Concerns\WithValidation;
 class LeadImport implements ToModel, WithValidation
 {
     use Importable;
+
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
-      
-        if(count($row) > 0){
-            //dd($row);
-            // Validator::make($rows->toArray(), [
-            //     '*.0' => 'required',
-            // ])->validate();
-
-            // $this->validate($row->toArray(), [
-            //     '*.0' => 'required'
-                
-            // ]);
-
-          
+        if (count($row) > 0) {
+            $exist = Lead::where('email', $row[1])->first();
+            if ($row[1] !== '' && $exist === null && $row[1] !== null) {
                 return new Lead([
                     'full_name' => $row[0],
                     'email' => @$row[1],
                     'phone' => @$row[2],
                     'organisation' => @$row[3],
                 ]);
-            
-   
-      
-           
-        }else{
+            }
+
+        } else {
             return back()->with('error', 'one of your excel sheet is empty');
         }
-       
+
     }
 
     public function rules(): array
     {
         return [
-            '0' => function($attribute, $value, $onFailure) {
+            '0' => function ($attribute, $value, $onFailure) {
                 if ($value == '') {
-                     $onFailure('Full Name is required');
+                    $onFailure('Full Name is required');
                 }
-            }, 
-            
+            },
+
         ];
-        }
+    }
 }
